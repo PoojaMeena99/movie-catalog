fe021e80395526ae751dd48afbf4b7371d05f107
Pooja Meena <your-email@example.com>
2024-07-03 04:07:44 +0530

    Enhanced functionality: added state management variables

--- app/page.js
+++ app/page.js
@@ -8,51 +8,76 @@ import Filter_panel from "./filter_panel";
 import Pagination from './pagination';
 
 function page() {
-
   const [currentPage, setCurrentPage] = useState(1);
   const [filteredItems, setFilteredItems] = useState(initial_data);
-  const itemsPerPage = 10;
+  const [release_year, setRelease_year] = useState("");
+  const [search_text, setSearch_text] = useState("");
+  const [rating, setRating] = useState("");
+  const [genre, setGenre] = useState("");
+  const [movie_length, setMovie_length] = useState("")
+  
+  const current_page_movies = filteredItems.slice((currentPage - 1) * 10, currentPage * 10);
+  const totalPages = Math.ceil(filteredItems.length / 10);
 
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const current_page_movies = filteredItems.slice(indexOfFirstItem, indexOfLastItem);
-
-  const totalPages = Math.ceil(filteredItems.length / itemsPerPage);
-
-  const handlePrevious = () => {
+  function handlePrevious() {
     if (currentPage > 1) {
       setCurrentPage(currentPage - 1);
     }
-  };
-
-  const handleNext = () => {
+  }
+  
+  function handleNext() {
     if (currentPage < totalPages) {
       setCurrentPage(currentPage + 1);
     }
-  };
-
+  }
+  
+  function handleReleaseYear() {
+    setRelease_year();
+  }
+  
+  function handleSearchText() {
+    setSearch_text();
+  }
+  
+  function handleGenre() {
+    setGenre();
+  }
+  
+  function handleRating() {
+    setRating();
+  }
+  
+  function handleMovieLength() {
+    setMovie_length();
+  }
+  
   useEffect(() => {
+    setFilteredItems(filtered);
+    setCurrentPage(1);
+  }, [filtered]);
+
+  const applyFunction = function () {
     const filtered = initial_data.filter(item =>
       item.release_year.toString().includes(year.toString())
     );
-    setFilteredItems(filtered);
-    setCurrentPage(1);
-}, [filtered]);
+  }
 
   return (
     <div className="container">
       <div className="row">
-        <Filter_panel 
-        filtered = {filtered }
+        <Filter_panel
+          handleReleaseYear={handleReleaseYear}
+          handleSearchText={handleSearchText}
+          handleMovieLength={handleMovieLength}
         />
-        <Movie_panel movie_list = {current_page_movies}/>
+        <Movie_panel movie_list={current_page_movies} />
       </div>
-      <Pagination 
-        handlePrevious = {handlePrevious}
-        handleNext = {handleNext}
-        currentPage = {currentPage}
-        totalPages = {totalPages}
-       />
+      <Pagination
+        handlePrevious={handlePrevious}
+        handleNext={handleNext}
+        currentPage={currentPage}
+        totalPages={totalPages}
+      />
     </div>
   );
 };
